8298504f2a50533c596664b6b1af8ae3b339f2fc
{Happy <happyswaraj7667@gmail.com>
2025-06-13 14:36:15 +0530

    string

--- Day02/string.js
+++ Day02/string.js
@@ -4,7 +4,26 @@
 // let a = "happy swaraj";  // its also count the space 
 // console.log(a, a.length);
 
-let s = "Rudra Partap Singh Rajput";
-for (let i=0; i<s.length; i++){
-    console.log(s[i])
-}
+// let s = "Rudra Partap Singh Rajput";
+// for (let i=0; i<s.length; i++){
+//     console.log(s[i])
+// }
+
+// let s = "Happy Swaraj";
+// console.log(s,s.length);
+// console.log(s.indexOf('w'));
+// console.log(s.lastIndexOf('p'));
+// console.log(s.charAt(4));
+
+
+
+// let s = "Happy Swaraj";
+// console.log(s.slice(1));   //substring from index 1 to end.
+// console.log(s.slice(1,6)); //substring from index 1 to 6.
+
+
+let s = "Happy Swaraj is a new student of LPU";
+let arr = s.split(' ');
+console.log(arr);
+
+
