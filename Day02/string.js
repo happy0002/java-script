fd80d091354d65cc2f4f0d906f5048095229c2db
{Happy <happyswaraj7667@gmail.com>
2025-06-13 13:39:25 +0530

    string

--- Day02/string.js
+++ Day02/string.js
@@ -1,2 +1,10 @@
-let s = "happy";
-console.log(s,s.length);
+// let s = "happy";
+// console.log(s,s.length);
+
+// let a = "happy swaraj";  // its also count the space 
+// console.log(a, a.length);
+
+let s = "Rudra Partap Singh Rajput";
+for (let i=0; i<s.length; i++){
+    console.log(s[i])
+}
