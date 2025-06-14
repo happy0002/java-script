889d41055094f6f7344de0586e894484183310bd
{Happy <happyswaraj7667@gmail.com>
2025-06-14 14:26:21 +0530

    update

---
+++ Day03/object.js
@@ -0,0 +1,10 @@
+let x = {    // x is the object 
+    name: "happy swaraj",
+    age: 23,
+    percentage: 85,
+    'nick name': "bhamlu",
+    isMarried: false
+};
+console.log(x);
+console.log(x['name']);
+console.log(x.age);  // for printing a particular key..
