ce5cf7c1a09f9c870db614e5ce43a1384ef0b8f3
{Happy <happyswaraj7667@gmail.com>
2025-06-16 14:18:21 +0530

    filter

---
+++ Day03/filter.js
@@ -0,0 +1,43 @@
+// function fun(ele){
+//      if(ele%2!=0)
+//         return true;
+//      else
+//       return false;
+// }
+// arr = [1,2,3,4,6,7,8,9,10,11];
+// console.log(arr);
+// brr = arr.filter(fun);
+// console.log(brr);
+
+
+
+
+// arr = [1,2,3,4,6,7,8,9,10,11];
+// console.log(arr);
+// brr = arr.filter(function fun(ele){
+//      if(ele%2===0)
+//         return true;
+//      else
+//       return false;
+// });
+// console.log(brr);
+
+
+
+
+arr = [1,2,3,4,6,7,8,9,10,11];
+console.log(arr);
+brr = arr.filter((ele)=>{
+     if(ele>5)
+        return true;
+     else
+      return false;
+});
+console.log(brr);
+
+
+
+
+
+
+    
